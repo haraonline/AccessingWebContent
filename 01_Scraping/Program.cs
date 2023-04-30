d3b4d51a51dd56b33ee4f21c5d30d597007994e3
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-30 21:22:53 +0200

    Add project files.

---
+++ 01_Scraping/Program.cs
@@ -0,0 +1,17 @@
+﻿using HtmlAgilityPack;
+
+namespace _01_Scraping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://en.wikipedia.org/wiki/Main_Page";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var node = doc.DocumentNode.SelectSingleNode("//head//title");
+            Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+        }
+    }
+}
