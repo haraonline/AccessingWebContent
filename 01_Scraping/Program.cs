f3247479976b0f1d6e071cc40ae851f83a413229
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-05-02 14:01:22 +0200

    clean

--- 01_Scraping/Program.cs
+++ 01_Scraping/Program.cs
@@ -6,12 +6,7 @@ namespace _01_Scraping
     {
         static void Main(string[] args)
         {
-            string url = "https://en.wikipedia.org/wiki/Main_Page";
-            var web = new HtmlWeb();
-            var doc = web.Load(url);
-
-            var node = doc.DocumentNode.SelectSingleNode("//head//title");
-            Console.WriteLine("Node Name: " + node.Name + "\n" + node.OuterHtml);
+            
         }
     }
 }
